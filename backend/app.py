39057da5bece68bbc3bbee482833df4a6ddbc743
lionard <leonard.muhati@student.moringaschool.com>
2024-12-16 20:12:52 +0300

    compelete

--- backend/app.py
+++ backend/app.py
@@ -7,10 +7,11 @@ from flask_cors import CORS
 
 from flask_jwt_extended import JWTManager
 from views import *
-
-port = int(os.environ.get("PORT", 5000))
 import os
 from app import app
+
+
+port = int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///business.db'
